582d862fcbc235d31f86056f3dfc8bdd1a9d4b86
OpenNeptune <neptunepos@gmail.com>
2014-06-15 18:05:00 +0800

    shit

--- J2EE/src/webServices/base/push/puslWebServices.java
+++ J2EE/src/webServices/base/push/puslWebServices.java
@@ -12,6 +12,9 @@ import webServices.base.serviceImpl.SystemInfoImpl;
  */
 public class puslWebServices {
 	public static void main(String[] args){
+		//此处使用纯JDK发布和使用cxf分布的效果是不一样，比如wsdl文档 纯JDK会分俩个文件
+		//而cxf则整合到一个文档中
+		
 		//webservices-helloWS
 		String address = "http://localhost:8080/J2EE/services/hellows";
 		String address1 = "http://localhost:8080/J2EE/services/systeminfo";
