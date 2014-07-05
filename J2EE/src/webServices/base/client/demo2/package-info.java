e276c932cb925761054531aa6982f4e98718f44d
OpenNeptune <neptunepos@gmail.com>
2014-07-05 14:35:04 +0800

    shit

--- J2EE/src/webServices/base/client/demo2/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://service.base.webServices/")
-package webServices.base.client.demo2;
--- J2EE/src/webServices/cxf/client/demo/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://inter.serv.cxf.webServices/")
-package webServices.cxf.client.demo;
